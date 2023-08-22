dc77662551321f5a76836bcd46988c0c8b9efeb5
RotemDevAcc <guyron2000@gmail.com>
2023-08-22 13:37:17 +0300

    Labels for better understanding of the code, update option, more colors and bug fixes

--- app.py
+++ app.py
@@ -2,38 +2,55 @@ from enum import Enum
 from functions import *
 
 
+# הגדרת מערך
 Animals = []
+
+# בולן שאחראי על להריץ את כל התהליך או לסיים אותו
 Running = False
 # class syntax
 class Actions(Enum):
     ADD = 1
     DELETE = 2
-    SEARCH = 3
-    LIST = 4
-    LISTACTIONS = 5
-    CLEAR = 6
-    STOP = 7
+    UPDATE = 3
+    SEARCH = 4
+    LIST = 5
+    LISTACTIONS = 6
+    CLEAR = 7
+    STOP = 8
 
 
 
-
+# מתחיל את התהליך
 def StartItUp():
+    # משתמש בבולן שלנו מלמעלה
     global Running
+
+    # מגדיר את הערך של הבולן להיות ההפך ממה שהוא כרגע
     Running = not Running
 
+    # עוצרים false אם הבולן שווה ל
     if(not Running):
         return
+    
+    # מדפיס את כל הפעולות האפשריות
     DrawActions()
+    # True שווה ל RUNNING כל עוד
     while (Running):
+        # מבקש פעולה דרך המקלדת
         action = Input()
+        # אם אין מתחילים מחדש כדי להימנע מתקלות
         if(action == None):
             continue
+        # ENUM אם יש בודק אותם דרך ה 
         action = Actions(int(action))
         if action == Actions.ADD:
             AddAnimal(Animals)
 
         if action == Actions.DELETE:
             DeleteAnimal(Animals)
+
+        if action == Actions.UPDATE:
+            UpdateAnimal(Animals)
 
         if action == Actions.SEARCH:
             SearchAnimal(Animals)
@@ -48,13 +65,19 @@ def StartItUp():
             ClearConsole()
 
         if action == Actions.STOP:
+            print("\033[91mKilling Program\033[0m")
             StartItUp()
 
 
+# מדפיס את כל האפשרויות
 def DrawActions():
+    # ומדפיס אותם בצבע ורוד Enums עובר על כל ה 
     for action in Actions:
-        print(f"{action.name} - {action.value}")
+        print(f"\033[95m{action.name} - {action.value}\033[0m")
 
+# כשהתוכנה עולה
 if __name__ == "__main__":
+    # טוען את החיות מהקובץ
     Animals = LoadAnimalsFromXML()
+    # מפעיל את התהליך
     StartItUp()
--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
 import os
 import xml.etree.ElementTree as ET
 
+# שם הקובץ
 filename = "animals.xml"
 
+# מנקה את הקונסול
 def ClearConsole():
     os.system("cls" if os.name == "nt" else "clear")
 
+# מחפש משהו במערך על פי שם
 def SearchKeyByName(name,table):
     for value in table:
         if(name == value["name"]):
@@ -13,19 +16,25 @@ def SearchKeyByName(name,table):
         
     return None
 
+# מבקש טקסט
 def Input():
     sendinput = input("Choose Your Action: ")
 
+    # NONE אם לא צויין שום דבר מחזירים 
     if(sendinput == None or sendinput == ""):
         print("\033[93mNo Action Chosen.\033[0m")
         return None
 
+    # אם הפעולה שנבחרה יכולה להיות מספר שולחים אותה
     if(sendinput.strip().isdigit()): return sendinput
     else: 
+        # אם לא אז כותבים בצבע צהוב שחייב להשתמש במספרים
         print("\033[93mYou Have To Choose A Number.\033[0m")
         return None
     
+# מדפיס את כל החיות עם הפרטים שלהם
 def ListAnimals(table):
+    # אם אין חיות לא להדפיס
     if len(table) <= 0:
         print("\033[93mNo Animals Found\033[0m")
         return
@@ -33,50 +42,103 @@ def ListAnimals(table):
     for animal in table:
         print(f"Name: {animal['name']}, Age: {animal['age']} Years Old, {animal['nickname']}\n")
         
+# מחפש חיות על פי המערך הקיים
 def SearchAnimal(table):
     animalname = input("Enter Animal Name [Example: Shark]: ")
+    # מחפש חיה על פי השם שהוגדר למעלה
     animal = SearchKeyByName(animalname,table)
+
+    # אם החיה לא נמצאה
     if(animal == None):
         print(f"Animal {animalname} Not Found")
 
+    # מדפיסים את החיה שחיפשנו עם כל הפרטים שלה
     print(f"Name: {animal['name']}, Age: {animal['age']}, Nickname: {animal['nickname']}")
 
+# הוספת חיות
 def AddAnimal(table):
     animalname = input("Enter Animal Name [Example: Shark]: ")
     animalage = input("Enter Animal Age [Example: 25]: ")
+
+    # אם הגיל של החיה שהוגדר אינו מספר
     if(not animalage.strip().isdigit()):
         print("\033[93mAnimal Age must be a Number\033[0m")
         return
     
     animalnickname = input("Enter Animal Nickname [Example: Megaladon]: ")
 
-
+    # אם צויין שם, גיל ושם כינוי
     if(animalname and animalage and animalnickname):
 
+        # אם השם כבר בשימוש לעצור כדי למנוע שמות כפולים
         if(SearchKeyByName(animalname,table)):
             print(f"\033[93mAnimal Name: {animalname} Is Already Used\033[0m")
             return
         
+        # מוסיפים למערך
         table.append({"name" : animalname, "age" : animalage, "nickname": animalnickname})
+        # מעדכנים את הקובץ
         SaveAnimalsToXML(table)
+        # מדפיסים איזה חיה נוספה ואת הפרטים שלי
         print(f"Added Animal Was Added, Name: {animalname}, Age: {animalage}, Nickname: {animalnickname}\n")
     else:
+        # אם לא
         print(f"Error: Not Enough Arguments Wanted 3 Got Less")
 
+# מחיקת חיות
 def DeleteAnimal(table):
     animalname = input("Enter Animal Name [Example: Shark]: ")
+    # אם לא צויין שם חיה
     if(not animalname): return
 
-    value = SearchKeyByName(animalname,table)
-    if(value == None):
+
+    animal = SearchKeyByName(animalname,table)
+    # אם החיה לא נמצאה
+    if(animal == None):
         return print("Animal Not Found")
     
-    table.remove(value)
+    # מוחקים מהמערך
+    table.remove(animal)
+    # שומרים לקובץ
     SaveAnimalsToXML(table)
             
 
+# מעדכן חיה קיימת
+def UpdateAnimal(table):
+    animalname = input("Enter Animal Name to Update: ")
+    animal = SearchKeyByName(animalname, table)
+    # אם לא נמצאה חיה
+    if animal == None:
+        print(f"\033[93mAnimal {animalname} Not Found\033[0m")
+        return
+
+    # מדפיס את שם החיה שכרגע אנו עוסקים בה
+    print(f"\033[92mCurrent Animal Data: Name: {animal['name']}, Age: {animal['age']}, Nickname: {animal['nickname']}\033[0m")
+
+    new_name = input("Enter New Animal Name [Leave Empty To Keep Current]: ")
+    new_age = input("Enter New Animal Age [Leave Empty To Keep Current]: ")
+    new_nickname = input("Enter New Animal Nickname [Leave Empty To Keep Current]: ")
+
+    # אם צוין שם מחליפים את הנוכחי בחדש
+    if new_name:
+        animal['name'] = new_name
+    # אם צוין גיל מחליפים את הנוכחי בחדש
+    if new_age and new_age.isdigit():
+        animal['age'] = new_age
+    # אם צוין שם כינוי מחליפים את הנוכחי בחדש
+    if new_nickname:
+        animal['nickname'] = new_nickname
+
+    # שומרים לקובץ
+    SaveAnimalsToXML(table)
+    # מעדכנים שהפעולה בוצעה בהצלחה
+    print(f"\033[92mAnimal {animalname} Updated Successfully\033[0m")
+
+
+# שומר את המערך לקובץ
 def SaveAnimalsToXML(table):
     root = ET.Element("zoo")
+    # צריך XML עובר על המערך ומסר אותו לפי מה ש
     for animal in table:
         animal_element = ET.Element("animal")
         name_element = ET.Element("name")
@@ -95,9 +157,13 @@ def SaveAnimalsToXML(table):
 
     print(f'table saved to {filename}')
 
+
+# טוען את החיות מהקובץ
+
 def LoadAnimalsFromXML():
     table = []
     table.clear()
+    # מנסה לגשת לקובץ
     try:
         tree = ET.parse(filename)
         root = tree.getroot()
@@ -112,9 +178,13 @@ def LoadAnimalsFromXML():
         
         print(f'table loaded from {filename}')
         return table
+    
+    # אם הקובץ לא נמצא
     except FileNotFoundError:
         print(f'File not found: {filename}')
         return []
+    
+    # אם לא עבד מכל סיבה אחרת
     except Exception as e:
         print(f'An error occurred while loading data: {str(e)}')
         return []
